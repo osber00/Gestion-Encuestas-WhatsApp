124cb0f98efce23bf3c689e7d27ee8112aed9c21
Oscar Bertel Peralta <oscar.bertelp@cecar.edu.co>
2024-02-23 15:24:43 -0500

    Traducción de correo (restaurar contraseña)

---
+++ resources/lang/es/reset.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'intro' => 'Recibes este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para tu cuenta.',
+    'action' => 'Restablecer Contraseña',
+    'outro' => 'Si no solicitaste un restablecimiento de contraseña, no se requiere ninguna acción adicional.',
+];
